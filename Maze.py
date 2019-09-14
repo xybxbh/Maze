7961753a1d37bd7adfeee2f7d7a29aa0c30956a7
xybxbh <xybxbh@gmail.com>
2019-09-14 13:36:09 -0400

    bfs, dfs

--- Maze.py
+++ Maze.py
@@ -35,12 +35,37 @@ class Maze(object):
                 path[(cur_x, cur_y - 1)] = (cur_x, cur_y)
                 nodes.append((cur_x, cur_y - 1))
         return False
-        
+
+    def dfs_solution(self):
+        nodes = [(0, 0)]
+        path = {}
+        while nodes:
+            (cur_x, cur_y) = nodes.pop()
+            if cur_x == self.dim - 1 and cur_y == self.dim - 1:
+                path_list = [(self.dim - 1, self.dim - 1)]
+                while path_list[-1] != (0, 0):
+                    path_list.append(path[path_list[-1]])
+                path_list.reverse()
+                return path_list
+            if cur_x + 1 < self.dim and self.env[cur_x + 1][cur_y] == 0 and (cur_x + 1, cur_y) not in path:
+                path[(cur_x + 1, cur_y)] = (cur_x, cur_y)
+                nodes.append((cur_x + 1, cur_y))
+            if cur_x - 1 >= 0 and self.env[cur_x - 1][cur_y] == 0 and (cur_x - 1, cur_y) not in path:
+                path[(cur_x - 1, cur_y - 1)] = (cur_x, cur_y)
+                nodes.append((cur_x - 1, cur_y))
+            if cur_y + 1 < self.dim and self.env[cur_x][cur_y + 1] == 0 and (cur_x, cur_y + 1) not in path:
+                path[(cur_x, cur_y + 1)] = (cur_x, cur_y)
+                nodes.append((cur_x, cur_y + 1))
+            if cur_y - 1 >= 0 and self.env[cur_x][cur_y - 1] == 0 and (cur_x, cur_y - 1) not in path:
+                path[(cur_x, cur_y - 1)] = (cur_x, cur_y)
+                nodes.append((cur_x, cur_y - 1))
+        return False
+
 if __name__ == "__main__":
     maze = Maze(0.1, 10)
     print(maze.env)
-    if maze.bfs_solution() == False:
+    if maze.dfs_solution() == False:
         print('pass')
     else:
-        for t in maze.bfs_solution():
+        for t in maze.dfs_solution():
             print(t)
