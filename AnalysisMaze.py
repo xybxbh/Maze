ccd6e0f7db22d9e616e06fd562fae0b474dbbd7d
Xiaochen <xcgao.shawn@gmail.com>
2019-09-22 23:18:13 -0400

    Fix Bug in bdbfs

--- AnalysisMaze.py
+++ AnalysisMaze.py
@@ -3,6 +3,7 @@ from Maze import *
 import matplotlib.pyplot as plt
 
 import datetime
+import time
 
 class AnalysisMaze(object):
 
@@ -23,12 +24,17 @@ class AnalysisMaze(object):
 
     def alg_runningtime(self, alg):
         while True:
-            starttime = datetime.datetime.now()
+            start = time.time()
             maze = Maze(self.occ_rate, self.dim)
-            solution_param = maze.solve(alg)
+            if alg == "a*EU":
+                solution_param = maze.solve("a*", "Euclidean")
+            elif alg == "a*MH":
+                solution_param = maze.solve("a*")
+            else:
+                solution_param = maze.solve(alg)
             if solution_param.has_path:
-                endtime = datetime.datetime.now()
-                print((endtime - starttime).seconds)
+                end = time.time()
+                print(end - start)
                 break
 
     def print(self, output):
@@ -42,12 +48,13 @@ class AnalysisMaze(object):
 
 
 def cal_runningtime_list():
-    alg = ["dfs", "bfs", "a*", "bdbfs"]
+    alg = ["dfs", "bfs", "a*EU", "a*MH", "bdbfs"]
     p = [0, 0.1, 0.2, 0.3]
-    dim = [10, 50, 100, 250, 500, 100]
+    dim = [10, 50, 100, 250, 500, 1000]
     for i in range(0, len(alg)):
         for j in range(0, len(p)):
             for k in range(0, len(dim)):
+                print("p: " + str(p[j]) + " dim: " + str(dim[k]) + " alg: " + alg[i])
                 a = AnalysisMaze(p[j], dim[k])
                 a.alg_runningtime(alg[i])
 
